4f77f1726b80ede54fdaa8b761eb402d70c00ac6
OlegLoshkin <loshkin.oleg.95@gmail.com>
2022-11-24 13:19:37 +0100

    first commit

---
+++ Source/MyAiProject/MyAiProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyAiProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyAiProject, "MyAiProject" );
---
+++ Source/MyAiProject/MyAiProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyAiProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYAIPROJECT_API AMyAiProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
